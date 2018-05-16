41d19197c72901501fa7dcd4c8ed27f336de56f9
toto <toto@gmailcom>
2018-05-16 15:25:17 +0200

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	deleted:    src/app/components/home/home.component.html 	deleted:    src/app/components/home/home.component.scss 	deleted:    src/app/components/home/home.component.ts 	deleted:    src/app/components/login/login.component.html 	deleted:    src/app/components/login/login.component.scss 	deleted:    src/app/components/login/login.component.ts 	modified:   src/app/components/navbar/navbar.component.html 	modified:   src/index.html 	src/app/components/page-calendar-models/ 	src/app/components/page-history/ 	src/app/components/page-home/ 	src/app/components/page-independant-modules/ 	src/app/components/page-login/ 	src/app/components/page-modules-requirement/ 	src/app/components/page-parameters/ 	src/app/components/page-search/

--- src/main/alter-app-client/src/app/app-routing.module.ts
+++ src/main/alter-app-client/src/app/app-routing.module.ts
@@ -1,22 +1,63 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./components/login/login.component";
+import {PageLoginComponent} from "./components/page-login/page-login.component";
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
-import {HomeComponent} from "./components/home/home.component";
+import {PageHomeComponent} from "./components/page-home/page-home.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {GuestGuard} from "./guards/guest.guard";
+import { PageSearchComponent } from './components/page-search/page-search.component';
+import { PageHistoryComponent } from './components/page-history/page-history.component';;
+import { PageParametersComponent } from './components/page-parameters/page-parameters.component';
+import { PageCalendarModelsComponent } from './components/page-calendar-models/page-calendar-models.component';
+import { PageIndependantModulesComponent } from './components/page-independant-modules/page-independant-modules.component';
+import { PageModulesRequirementComponent } from './components/page-modules-requirement/page-modules-requirement.component';
 
 // Define the routes
 const ROUTES: Routes = [
-  {
+  { 
     path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     canActivate: [AuthGuard],
-    component: HomeComponent
+    component: PageHomeComponent
   },
   {
     path: 'login',
     canActivate: [GuestGuard],
-    component: LoginComponent
+    component: PageLoginComponent
+  },
+  {
+    path: 'search',
+    canActivate: [AuthGuard],
+    component: PageSearchComponent
+  },
+  {
+    path: 'calendar-models',
+    canActivate: [AuthGuard],
+    component: PageCalendarModelsComponent
+  },
+  {
+    path: 'independant-modules',
+    canActivate: [AuthGuard],
+    component: PageIndependantModulesComponent
+  },
+  {
+    path: 'modules-requirement',
+    canActivate: [AuthGuard],
+    component: PageModulesRequirementComponent
+  },
+  {
+    path: 'history',
+    canActivate: [AuthGuard],
+    component: PageHistoryComponent
+  },
+  {
+    path: 'parameters',
+    canActivate: [AuthGuard],
+    component: PageParametersComponent
   },
   {
     path: '**',
--- src/main/alter-app-client/src/app/app.module.ts
+++ src/main/alter-app-client/src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from './app.component';
-import {LoginComponent} from './components/login/login.component';
+import {PageLoginComponent} from './components/page-login/page-login.component';
 import {AuthService} from "./services/auth.service";
 import {UserService} from "./services/user.service";
-import {HomeComponent} from './components/home/home.component';
+import {PageHomeComponent} from './components/page-home/page-home.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule} from "@angular/forms";
@@ -14,15 +14,27 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {CourService} from "./services/cour.service";
 import {PromotionService} from "./services/promotion.service";
+import { PageSearchComponent } from './components/page-search/page-search.component';
+import { PageHistoryComponent } from './components/page-history/page-history.component';
+import { PageParametersComponent } from './components/page-parameters/page-parameters.component';
+import { PageCalendarModelsComponent } from './components/page-calendar-models/page-calendar-models.component';
+import { PageModulesRequirementComponent } from './components/page-modules-requirement/page-modules-requirement.component';
+import { PageIndependantModulesComponent } from './components/page-independant-modules/page-independant-modules.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
+    PageLoginComponent,
+    PageHomeComponent,
     PageNotFoundComponent,
     NavbarComponent,
+    PageSearchComponent,
+    PageHistoryComponent,
+    PageParametersComponent,
+    PageCalendarModelsComponent,
+    PageModulesRequirementComponent,
+    PageIndependantModulesComponent,
   ],
   imports: [
     NgbModule.forRoot(),
