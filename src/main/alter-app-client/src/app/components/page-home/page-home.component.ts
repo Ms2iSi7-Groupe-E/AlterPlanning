b221a57c2a2635058bf3ea6d747fbf19206a595f
toto <toto@gmailcom>
2018-05-16 16:11:13 +0200

    mise en place des pages

--- src/main/alter-app-client/src/app/components/page-home/page-home.component.ts
+++ src/main/alter-app-client/src/app/components/page-home/page-home.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../services/user.service";
-import {PromotionService} from "../../services/promotion.service";
 
 @Component({
   selector: 'app-home',
@@ -8,43 +6,11 @@ import {PromotionService} from "../../services/promotion.service";
   styleUrls: ['./page-home.component.scss']
 })
 export class PageHomeComponent implements OnInit {
-  user = {};
-  promotions = [];
-  selectedPromotion;
-  cours = [];
 
-  constructor(private userService: UserService,
-              private promotionService: PromotionService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.userService.getMe().subscribe(
-      res => {
-        this.user = res;
-      },
-      err => {
-        console.error(err);
-      }
-    );
 
-    this.promotionService.getPromotions().subscribe(
-      res => {
-        this.promotions = res;
-      },
-      err => {
-        console.error(err);
-      }
-    );
-  }
-
-  changePromotion() {
-    this.promotionService.getCourByCodePromotion(this.selectedPromotion.codePromotion).subscribe(
-      res => {
-        this.cours = res;
-      },
-      err => {
-        console.error(err);
-      }
-    );
   }
 
 }
