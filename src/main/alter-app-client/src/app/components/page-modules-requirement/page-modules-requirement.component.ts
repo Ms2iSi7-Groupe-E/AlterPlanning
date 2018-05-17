6790c0a10104fc11f77957827dcddfecf61c80fa
toto <toto@gmailcom>
2018-05-17 20:45:31 +0200

    dev des pre-requis

--- src/main/alter-app-client/src/app/components/page-modules-requirement/page-modules-requirement.component.ts
+++ src/main/alter-app-client/src/app/components/page-modules-requirement/page-modules-requirement.component.ts
@@ -7,6 +7,7 @@ import { ModuleService } from '../../services/module.service';
   styleUrls: ['./page-modules-requirement.component.scss']
 })
 export class PageModulesRequirementComponent implements OnInit {
+  dataModules = [];
   sourceTitle = '';
   sourceFormation = '';
   sourceModule = '';
@@ -22,77 +23,90 @@ export class PageModulesRequirementComponent implements OnInit {
 
   ngOnInit() {
 
+    // recuperation de la liste des modules
+    this.moduleService.getModules().subscribe(
+      res => {
+        this.dataModules = res;
+        this.sourcesModules = res;
+        this.targetModules = res;
+      },
+      err => {
+        console.error(err);
+      }
+    );
   }
 
   findModuleSource(){
 
     // controle de la taille des saisies
-    if( this.sourceTitle.length < 3 && this.sourceFormation.length < 3 && this.sourceModule.length < 3 ){
+    if( this.sourceTitle.length < 1 && this.sourceFormation.length < 1 && this.sourceModule.length < 1 ){
+      this.sourcesModules = this.dataModules;
       return;
     }
 
-    this.moduleService.getModules().subscribe(
-      res => {
+    // filtre les resultats a afficher
+    let showRes = [];
+    for (let item of this.dataModules) {
 
-        // filtre les resultats a afficher
-        let showRes = [];
-        for (let item of res) {
-
-          // filtre sur le libelle du module
-          if( this.sourceModule.length > 2 && item.Libelle.toLowerCase().indexOf( this.sourceModule.toLowerCase() ) == -1 ){
-            continue;
-          }
-
-          //TODO: les autres criteres
-
-          showRes.push(item);
-
-          // si la taille maximum est atteinte
-          if( showRes.length == 5 ){
-            break;
-          }
-        }
-        this.sourcesModules = showRes;
-      },
-      err => {
-        console.error(err);
+      // filtre sur le libelle du module
+      if( this.sourceModule.length > 1 && item.libelle.toLowerCase().indexOf( this.sourceModule.toLowerCase() ) == -1 ){
+        continue;
       }
-    );
+
+      //TODO: les autres criteres
+
+      showRes.push(item);
+
+      // si la taille maximum est atteinte
+      if( showRes.length == 5 ){
+        break;
+      }
+    }
+    this.sourcesModules = showRes;
+  }
+
+  filterTargetModules(modules){
+    let lstModules = [];
+    for (let item of modules) {
+      if( this.sourceSelected != null && ( this.sourceSelected.idModule == item.idModule ) ){
+        continue;
+      }
+      lstModules.push(item);
+    }
+    return lstModules;
   }
 
   findModuleTarget(){
 
     // controle de la taille des saisies
-    if( this.targetTitle.length < 3 && this.targetFormation.length < 3 && this.targetModule.length < 3 ){
+    if( this.targetTitle.length < 1 && this.targetFormation.length < 1 && this.targetModule.length < 1 ){
+      this.targetModules = this.dataModules;
       return;
     }
 
-    this.moduleService.getModules().subscribe(
-      res => {
+    // filtre les resultats a afficher
+    let showRes = [];
+    for (let item of this.dataModules) {
 
-        // filtre les resultats a afficher
-        let showRes = [];
-        for (let item of res) {
-
-          // filtre sur le libelle du module
-          if( this.targetModule.length > 2 && item.Libelle.toLowerCase().indexOf( this.targetModule.toLowerCase() ) == -1 ){
-            continue;
-          }
-
-          //TODO: les autres criteres
-
-          showRes.push(item);
-
-          // si la taille maximum est atteinte
-          if( showRes.length == 4 ){
-            break;
-          }
-        }
-        this.targetModules = showRes;
-      },
-      err => {
-        console.error(err);
+      // filtre sur le libelle du module
+      if( this.targetModule.length > 1 && item.libelle.toLowerCase().indexOf( this.targetModule.toLowerCase() ) == -1 ){
+        continue;
       }
-    );
+
+      // si c'est le module source
+      if( this.sourceSelected != null && this.sourceSelected.idModule == item.idModule ){
+        continue;
+      }
+
+      //TODO: les autres criteres
+
+      showRes.push(item);
+
+      // si la taille maximum est atteinte
+      if( showRes.length == 4 ){
+        break;
+      }
+    }
+    this.targetModules = showRes;
   }
 }
