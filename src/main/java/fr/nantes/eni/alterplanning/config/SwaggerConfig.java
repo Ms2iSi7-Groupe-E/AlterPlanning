6a017f21d15886e0695414c6b8ec27f8b8e9caab
Ugho STEPHAN <stephan.ugho@gmail.com>
2018-05-19 11:10:24 +0200

    Ok with auth swagger

--- src/main/java/fr/nantes/eni/alterplanning/config/SwaggerConfig.java
+++ src/main/java/fr/nantes/eni/alterplanning/config/SwaggerConfig.java
@@ -1,16 +1,31 @@
 package fr.nantes.eni.alterplanning.config;
 
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.ApiListingScannerPlugin;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    private static final String AUTHENTICATION_NAME = "Authorization (Bearer Token)";
 
     @Bean
     public Docket api() {
@@ -18,6 +33,27 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("fr.nantes.eni.alterplanning.controller.api"))
                 .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Collections.singletonList(securityContexts()))
+                .securitySchemes(Collections.singletonList(securitySchemes()));
+    }
+
+    private SecurityScheme securitySchemes() {
+        return new ApiKey(AUTHENTICATION_NAME, tokenHeader, "header");
+    }
+
+    private SecurityContext securityContexts() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(Predicates.not(PathSelectors.regex("/api/auth")))
                 .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference(AUTHENTICATION_NAME, authorizationScopes));
     }
 }
