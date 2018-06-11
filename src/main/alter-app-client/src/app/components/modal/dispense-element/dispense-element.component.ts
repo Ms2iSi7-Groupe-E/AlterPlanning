c598520572dee52b20644203a7dda591c32a64ab
Ugho STEPHAN <stephan.ugho@gmail.com>
2018-06-11 22:59:57 +0200

    modals page home OK

--- src/main/alter-app-client/src/app/components/modal/dispense-element/dispense-element.component.ts
+++ src/main/alter-app-client/src/app/components/modal/dispense-element/dispense-element.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {StagiaireService} from "../../../services/stagiaire.service";
+import {ModuleService} from "../../../services/module.service";
 
 @Component({
   selector: 'app-dispense-element',
@@ -14,16 +16,59 @@ export class DispenseElementComponent implements OnInit {
   private readonly DISPENSE_MODULE = 'DISPENSE_MODULE';
   private readonly PAS_EN_MEME_TEMPS_QUE = 'PAS_EN_MEME_TEMPS_QUE';
 
-  constructor(private activeModal: NgbActiveModal) {}
+  modules = [];
+  stagiaires = [];
+
+  selectedModule = null;
+  selectedStagiaire = null;
+  selectedPeriodeDebut = null;
+  selectedPeriodeFin = null;
+
+  constructor(private activeModal: NgbActiveModal,
+              private moduleService: ModuleService,
+              private stagiaireService: StagiaireService) {}
 
   ngOnInit() {
+    this.moduleService.getModules().subscribe(res => {
+        this.modules = res;
+      },
+      err => {
+        console.error(err);
+      });
+
+    this.stagiaireService.getStagiaires().subscribe(res => {
+        this.stagiaires = res;
+      },
+      err => {
+        console.error(err);
+      });
   }
 
-  dispenseElement() {
-    this.dispense.emit({
-      name: 'PAS_EN_MEME_TEMPS_QUE',
-      value: 211
-    });
+  dispenseElement(type) {
+    let value = null;
+
+    switch (type) {
+      case this.DISPENSE_MODULE:
+        value = this.modules.find(m => m.idModule === this.selectedModule);
+        break;
+
+      case this.PAS_EN_MEME_TEMPS_QUE:
+        value = this.stagiaires.find(s => s.codeStagiaire === this.selectedStagiaire);
+        break;
+
+      case this.DISPENSE_PERIODE:
+        value = {
+          from: new Date(this.selectedPeriodeDebut.year, this.selectedPeriodeDebut.month - 1, this.selectedPeriodeDebut.day),
+          to: new Date(this.selectedPeriodeFin.year, this.selectedPeriodeFin.month - 1, this.selectedPeriodeFin.day)
+        };
+        break;
+
+      default:
+        return;
+    }
+
+    this.dispense.emit({ type, value });
+
     this.activeModal.dismiss('Cross click');
   }
 }
