4666ad110436be09de70c8960aa202c0a19c1cc1
Ugho STEPHAN <ugho.stephan@consertotech.pro>
2018-06-11 09:58:40 +0200

    Init Module requirement

--- src/main/java/fr/nantes/eni/alterplanning/controller/api/ModuleController.java
+++ src/main/java/fr/nantes/eni/alterplanning/controller/api/ModuleController.java
@@ -6,10 +6,7 @@ import fr.nantes.eni.alterplanning.exception.RestResponseException;
 import fr.nantes.eni.alterplanning.service.dao.CoursDAOService;
 import fr.nantes.eni.alterplanning.service.dao.ModuleDAOService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -43,5 +40,29 @@ public class ModuleController {
     @GetMapping("/{idModule}/cours")
     public List<CoursEntity> getCoursByModule(@PathVariable(name = "idModule") Integer idModule) {
         return coursDAOService.findByModule(idModule);
+    }
+
+    @GetMapping("/{idModule}/requirement")
+    public List<ModuleEntity> getRequirementByModule(@PathVariable(name = "idModule") Integer idModule) throws RestResponseException {
+        // TODO
+        throw new RestResponseException(HttpStatus.NOT_IMPLEMENTED, "Not yet implemented");
+    }
+
+    @PostMapping("/{idModule}/requirement")
+    public List<ModuleEntity> addRequirementForModule(@PathVariable(name = "idModule") Integer idModule) throws RestResponseException {
+        // TODO
+        throw new RestResponseException(HttpStatus.NOT_IMPLEMENTED, "Not yet implemented");
+    }
+
+    @PutMapping("/{idModule}/requirement")
+    public List<ModuleEntity> updateRequirementByModule(@PathVariable(name = "idModule") Integer idModule) throws RestResponseException {
+        // TODO
+        throw new RestResponseException(HttpStatus.NOT_IMPLEMENTED, "Not yet implemented");
+    }
+
+    @DeleteMapping("/{idModule}/requirement")
+    public List<ModuleEntity> deleteRequirementByModule(@PathVariable(name = "idModule") Integer idModule) throws RestResponseException {
+        // TODO
+        throw new RestResponseException(HttpStatus.NOT_IMPLEMENTED, "Not yet implemented");
     }
 }
