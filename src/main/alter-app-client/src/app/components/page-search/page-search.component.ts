23b1e04ca7b7e37b43188fca51e241a869279130
Ugho STEPHAN <stephan.ugho@gmail.com>
2018-06-12 23:14:44 +0200

    avancement search page

--- src/main/alter-app-client/src/app/components/page-search/page-search.component.ts
+++ src/main/alter-app-client/src/app/components/page-search/page-search.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CalendarService} from "../../services/calendar.service";
 import {CalendarStates} from "../../models/enums/calendar-states";
+import {StagiaireService} from "../../services/stagiaire.service";
+import {EntrepriseService} from "../../services/entreprise.service";
+import {ModuleService} from "../../services/module.service";
+import {PromotionService} from "../../services/promotion.service";
+import {FormationService} from "../../services/formation.service";
 
 @Component({
   selector: 'app-page-search',
@@ -9,20 +14,66 @@ import {CalendarStates} from "../../models/enums/calendar-states";
 })
 export class PageSearchComponent implements OnInit {
 
+  CALENDAR_STATE = CalendarStates;
+
+  stagiaires = [];
+  entreprises = [];
+  promotions = [];
+  modules = [];
+  formations = [];
+
+  selectedState = "ALL";
+  selectedStagiaire = null;
+  selectedEntreprise = null;
+  selectedModule = null;
+  selectedFormation = null;
+  selectedPromotion = null;
+  selectedDateDebut = null;
+  selectedDateFin = null;
+
   calendars = [];
 
-  constructor(private calendarService: CalendarService) { }
+  constructor(private calendarService: CalendarService,
+              private stagiaireService: StagiaireService,
+              private entrepriseService: EntrepriseService,
+              private moduleService: ModuleService,
+              private promotionService: PromotionService,
+              private formationService: FormationService) { }
 
   ngOnInit() {
-    // Get stagaires
-    // Get entreprises
-    // Get promotions
-    // Get cursus
-    // Get Modules
+    this.stagiaireService.getStagiaires().subscribe(res => {
+      this.stagiaires = res;
+    }, err => {
+      console.error(err);
+    });
+
+    this.entrepriseService.getEntreprises().subscribe(res => {
+      this.entreprises = res;
+    }, err => {
+      console.error(err);
+    });
+
+    this.moduleService.getModules().subscribe(res => {
+      this.modules = res;
+    }, err => {
+      console.error(err);
+    });
+
+    this.promotionService.getPromotions().subscribe(res => {
+      this.promotions = res;
+    }, err => {
+      console.error(err);
+    });
+
+    this.formationService.getFormations().subscribe(res => {
+      this.formations = res;
+    }, err => {
+      console.error(err);
+    });
   }
 
   search() {
-    // For the moment get all calendar for mock
+    // TODO : For the moment get all calendar for mock
     this.calendarService.getCalendars().subscribe(res => {
       this.calendars = res;
     }, err => {
