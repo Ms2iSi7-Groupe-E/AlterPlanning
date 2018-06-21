7ef469c17070cc3ec9d056c6e66c18ab65869c56
Ugho STEPHAN <stephan.ugho@gmail.com>
2018-06-21 11:56:07 +0200

    add info message login page

--- src/main/alter-app-client/src/app/components/page-login/page-login.component.ts
+++ src/main/alter-app-client/src/app/components/page-login/page-login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthModel} from "../../models/auth.model";
 import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -13,19 +13,37 @@ export class PageLoginComponent implements OnInit {
   errorMessage: String;
   errorList: String[];
   model: AuthModel;
+  infoMessage: String;
   submitted: boolean;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.submitted = false;
     this.model = new AuthModel();
     this.errorMessage = "";
     this.errorList = [];
+    this.handleInfoParam();
+  }
+
+  handleInfoParam() {
+    const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
+    this.infoMessage = queryParams.info ? queryParams.info : "";
+
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: {
+        ...this.activatedRoute.snapshot.queryParams,
+        info: null,
+      }
+    });
   }
 
   onSubmit() {
     this.submitted = true;
+    this.infoMessage = "";
     this.errorMessage = "";
     this.errorList = [];
 
