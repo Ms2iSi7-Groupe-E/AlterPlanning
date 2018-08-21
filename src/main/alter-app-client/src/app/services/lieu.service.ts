78da2f444f283bd7422b0884a36ada18cfb62c1b
toto <toto@gmailcom>
2018-08-21 17:50:39 +0200

    =dev de la visu de planning

--- src/main/alter-app-client/src/app/services/lieu.service.ts
+++ src/main/alter-app-client/src/app/services/lieu.service.ts
@@ -8,6 +8,12 @@ export class LieuService {
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
+  getLieuById(codeLieu: string): Observable<any> {
+    return this.http
+      .get('/api/lieu/' + codeLieu, {headers: AuthService.getHeaders()})
+      .catch((err) => this.authService.handleError(err));
+  }
+
   getLieuxTeachningCourses(): Observable<any> {
     return this.http
       .get('/api/lieu?with-courses=true', {headers: AuthService.getHeaders()})
