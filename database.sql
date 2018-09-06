19121733b5065bd6aece5a4c1296dd7c6b37bd25
Ugho STEPHAN <stephan.ugho@gmail.com>
2018-09-06 11:03:47 +0200

    add create database

--- database.sql
+++ database.sql
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `alter-planning`
 --
+CREATE DATABASE IF NOT EXISTS `alter-planning` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `alter-planning`;
 
 -- --------------------------------------------------------
 
